b50cf092a5cf30c1bc8515f46d688ef8f31ba853
emredursunn <emredursun908@gmail.com>
2024-08-04 12:04:50 +0300

    styled select component

--- src/components/body/LanguageSelector.tsx
+++ src/components/body/LanguageSelector.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Select, { SingleValue } from "react-select";
+import Select, { SingleValue, StylesConfig } from "react-select";
 
 export const LanguageSelector: React.FC = () => {
   const { i18n } = useTranslation();
@@ -10,46 +10,46 @@ export const LanguageSelector: React.FC = () => {
     { value: "tr", label: "Türkçe" },
   ];
 
-  // const customStyles: StylesConfig<{ value: string; label: string }> = {
-  //   option: (provided, state) => ({
-  //     ...provided,
-  //     color: "#fff",
-  //     backgroundColor: state.isFocused ? "#000" : "#333",
-  //     fontSize: "16px",
-  //     padding: "6px",
-  //     cursor: "pointer", // Cursor when hovering
-  //   }),
+  const customStyles: StylesConfig<{ value: string; label: string }> = {
+    option: (provided, state) => ({
+      ...provided,
+      color: "#fff",
+      backgroundColor: state.isFocused ? "#000" : "#333",
+      fontSize: "16px",
+      padding: "6px",
+      cursor: "pointer", // Cursor when hovering
+    }),
 
-  //   control: (provided) => ({
-  //     ...provided,
-  //     backgroundColor: "rgba(0, 0, 0, 0.8)",
-  //     padding: "2px",
-  //     border: "none",
-  //     boxShadow: "none",
-  //     borderRadius: 8,
-  //   }),
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+      padding: "2px",
+      border: "none",
+      boxShadow: "none",
+      borderRadius: 8,
+    }),
 
-  //   singleValue: (provided) => ({
-  //     ...provided,
-  //     color: "#fff",
-  //     fontSize: "16px",
-  //   }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "#fff",
+      fontSize: "16px",
+    }),
 
-  //   menu: (provided) => ({
-  //     ...provided,
-  //     borderRadius: 4,
-  //     backgroundColor: "#000",
-  //   }),
-  //   menuList: (provided) => ({
-  //     ...provided,
-  //     padding: 0, // Remove padding from menu list
-  //     borderRadius: 4,
-  //   }),
-  //   indicatorSeparator: (provided) => ({
-  //     ...provided,
-  //     display: "none",
-  //   }),
-  // };
+    menu: (provided) => ({
+      ...provided,
+      borderRadius: 4,
+      backgroundColor: "#000",
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      padding: 0, // Remove padding from menu list
+      borderRadius: 4,
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: "none",
+    }),
+  };
 
   const handleChangeLanguage = (
     selectedOption: SingleValue<{ value: string; label: string }>
@@ -63,7 +63,7 @@ export const LanguageSelector: React.FC = () => {
     <Select
       options={options}
       defaultValue={{ value: "en", label: "English" }}
-      //styles={customStyles}
+      styles={customStyles}
       onChange={handleChangeLanguage}
     />
   );
