17aed06a256bcf7453b1d7c695109beeb5bd73de
emredursunn <emredursun908@gmail.com>
2024-09-27 22:42:49 +0300

    return type is added

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,9 +1,10 @@
+import { Movie } from "../types/Movie";
 import { api } from "./api";
 
-export const getPopularMovies = async () => {
+export const getPopularMovies = async (): Promise<Movie[]> => {
   try {
     const response = await api.get("/movie/popular");
-    return response.data;
+    return response.data.results;  // Ensure the correct path to the movie data
   } catch (error: any) {
     console.error("Error fetching popular movies:", error.message);
     throw new Error(`Failed to fetch popular movies: ${error.message}`);
