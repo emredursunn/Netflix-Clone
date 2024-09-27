0daba4fdcf4412e57cdbcd60c0f14cf54201350d
emredursunn <emredursun908@gmail.com>
2024-09-27 22:45:29 +0300

    query client is initialized

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import "./i18n";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
   <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
   </Provider>
+  </QueryClientProvider>
 );
